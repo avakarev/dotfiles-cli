1734a2a5ddfe87bb1e82e1d0b8c66bbfb05b9f64
Andrii Vakarev <avakarev@gmail.com>
2021-02-08 02:36:06 +0100

    Refactor cmd/env

--- cmd/env.go
+++ cmd/env.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"text/tabwriter"
+
 	"github.com/spf13/cobra"
 
 	"github.com/avakarev/dotfiles-cli/internal/config"
-	"github.com/avakarev/dotfiles-cli/internal/printer"
 )
 
 // envCmd implements the `env` command
@@ -12,12 +15,15 @@ var envCmd = &cobra.Command{
 	Use:   "env",
 	Short: "Prints dotfiles env",
 	Run: func(cmd *cobra.Command, args []string) {
-		p := printer.Default(nil)
-		p.Addln("HomeDir", config.HomeDir)
-		p.Addln("WorkingDir", config.WorkingDir)
-		p.Addln("ConfigDir", config.ConfigDir)
-		p.Addln("ConfigFile", config.ConfigFile)
-		p.Flush()
+		w := tabwriter.NewWriter(log.Writer(), 0, 0, 2, ' ', 0)
+		f := "%s\t%s\t\n"
+
+		fmt.Fprintf(w, f, "HomeDir", config.HomeDir)
+		fmt.Fprintf(w, f, "WorkingDir", config.WorkingDir)
+		fmt.Fprintf(w, f, "ConfigDir", config.ConfigDir)
+		fmt.Fprintf(w, f, "ConfigFile", config.ConfigFile)
+
+		w.Flush()
 	},
 }
 
